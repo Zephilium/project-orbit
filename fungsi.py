82630ba20753dc90c48ca356897d592a962f0470
Zephilium <wuuuuush@gmail.com>
2021-12-26 21:58:08 +0700

    add speech to text

---
+++ fungsi.py
@@ -0,0 +1,360 @@
+import streamlit as st
+
+import en_core_web_sm
+
+import PyPDF2
+
+import pdfplumber
+
+from pyresparser import ResumeParser
+
+from pydub import AudioSegment
+
+import librosa
+
+import soundfile as sf
+import speech_recognition as sr
+
+
+def extract_data(feed):
+
+    data = []
+
+    with pdfplumber.load(feed) as pdf:
+
+        pages = pdf.pages
+
+        for p in pages:
+
+            data.append(p.extract_tables())
+
+    return None
+
+
+def ekstrak():
+
+    uploaded_file = st.file_uploader('Choose your .zip file', type="zip")
+    import zipfile
+
+    if uploaded_file is not None:
+        with zipfile.ZipFile(uploaded_file, "r") as zip_ref:
+            zip_ref.extractall("CV")
+
+    else:
+        pass
+
+
+def seleksi(f1, pil_posisi, p_posisi):
+
+    # posisi = 'Data analytics'
+
+    # uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+
+    if f1 is not None:
+
+        data = ResumeParser('CV/'+str(f1)).get_extracted_data()
+
+        skills = data['skills']
+
+        skills = []
+
+        for i in data['skills']:
+
+            skills.append(i.lower())
+
+        terms = {'UI / UX':             ['sketch', 'figma', 'invision', 'concept', 'drawing', 'user flow', 'diagram', 'wireframe', 'prototype',
+
+
+                                         'adobe', 'photoshop', 'illustrator', 'zeplin', 'xd', 'marvel'],
+
+
+                 'Back-end developer':   ['api', 'php', 'python', 'mysql', 'mariadb', 'git', 'codeigniter', 'express', 'scrum agile', 'java', 'laravel'],
+
+
+                 'Front-end developer':  ['html', 'css', 'javascript', 'node.js', 'react', 'react.js', 'vue.js', 'angular', 'bootstrap', 'git'],
+
+
+                 'Cloud Computing':      ['aws', 'azure', 'google', 'docker', 'vmware', 'cloud', 'server', 'storage', 'kubernetes', 'architect', 'migration',
+
+
+                                          'it infrastructure', 'network', 'ci', 'cd', 'jenkins', 'nginx', 'terraform'],
+
+
+                 'Data analytics':       ['analytics', 'api', 'aws', 'big data', 'busines intelligence', 'clustering', 'code',
+
+
+                                          'coding', 'data', 'database', 'data mining', 'data science', 'deep learning', 'hadoop',
+
+
+                                          'hypothesis test', 'iot', 'internet', 'machine learning', 'modeling', 'nosql', 'nlp',
+
+
+                                          'predictive', 'programming', 'python', 'r', 'sql', 'tableau', 'text mining',
+
+
+                                          'visualuzation', 'excel', 'spss', 'sas'],
+
+
+                 'Network Engineer':     ['routing', 'dns', 'wan', 'man', 'lan', 'firewall', 'tcp/ip', 'dhcp', 'vpn', 'networking'],
+
+
+                 'Mobile Developer':     ['kotlin', 'java', 'android', 'ios', 'sqlite', 'firebase', 'oop', 'flutter', 'api', 'swift', 'react', 'rest',
+
+
+                                          'json', 'solid', 'git', 'dart', 'android studio', 'xcode', 'rest api']}
+
+        ui = 0
+
+        backen = 0
+
+        frontend = 0
+
+        cloud = 0
+
+        analytics = 0
+
+        network = 0
+
+        mobile = 0
+
+        # # Create an empty list where the scores will be stored
+
+        scores = []
+
+        kemampuan = {}
+
+        # Obtain the scores for each area
+
+        for area in terms.keys():
+
+            kemampuan[area] = []
+
+            if area == 'UI / UX':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        ui += 1
+
+                scores.append(ui)
+
+            elif area == 'Back-end developer':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        backen += 1
+
+                scores.append(backen)
+
+            elif area == 'Front-end developer':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        frontend += 1
+
+                scores.append(frontend)
+
+            elif area == 'Cloud Computing':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        cloud += 1
+
+                scores.append(cloud)
+
+            elif area == 'Data analytics':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        analytics += 1
+
+                scores.append(analytics)
+
+            elif area == 'Network Engineer':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        network += 1
+
+                scores.append(network)
+
+            elif area == 'Mobile Developer':
+
+                for skill in terms[area]:
+
+                    if skill in skills:
+
+                        kemampuan[area].append(skill)
+
+                        mobile += 1
+
+                scores.append(mobile)
+
+        # p_posisi = ['Quality/Six Sigma', 'Operations management',
+
+        #             'Supply chain', 'Project management', 'Data analytics', 'Healthcare']
+
+        # pil_posisi = st.selectbox('Pilih Posisi', p_posisi)
+
+        # for i in p_posisi:
+
+        #     if i == pil_posisi:
+
+        #         st.write(f'Anda memilih {i}')
+
+        # st.write(
+
+        #     f'Kemampuan yang dimiliki oleh {data["name"]} tentang {pil_posisi} adalah ')
+
+        # st.write(data['name'])
+
+        ind = 0
+
+        job = {}
+
+        for x in terms.keys():
+
+            # st.write(f'{x}: {scores[ind]}')
+
+            job[x] = scores[ind]
+
+            ind += 1
+
+        a = {}
+
+        # a[data['name']] = [pil_posisi, job[pil_posisi], kemampuan[pil_posisi]]
+
+        job = {'nama': f1, 'posisi': pil_posisi,
+               'skor': job[pil_posisi], 'skil': kemampuan[pil_posisi]}
+
+        # st.write(job)
+
+        return job
+
+    else:
+
+        st.write('Silahkan Upload Data')
+
+
+def stt():
+
+    # song = AudioSegment.from_mp3('audio/aziz2.mp3')
+    upload_mp3 = st.file_uploader('Choose your .mp3 file', type="mp3")
+
+    if upload_mp3 is not None:
+
+        song = AudioSegment.from_mp3(upload_mp3)
+
+        newSong = song
+
+        newSong.export(f'output/{upload_mp3.name[0:-4]}.wav', format="wav")
+
+        # st.write(upload_mp3.name[0:-4])
+        audio_path = f'output/{upload_mp3.name[0:-4]}.wav'
+        x, sampleRate = librosa.load(audio_path)
+
+        pos = []
+        neg = []
+        for i in x:
+            if i > 0:
+                pos.append(i)
+            elif i < 0:
+                neg.append(i)
+
+        pos.sort()
+        neg.sort()
+
+        pos1 = int(0.3*len(pos))
+
+        neg1 = int(0.3*len(neg))
+
+        l = []
+
+        start = 0
+        end = 0
+        counter = 0
+        list_counter = []
+
+        for i in range(len(x)):
+            if neg[neg1] <= x[i] and x[i] <= pos[pos1]:
+                counter += 1
+
+            else:
+                if counter > 500:
+                    list_counter.append(counter)
+                    counter = 0
+
+            start = i
+
+            if counter == 5000:
+                end = i
+                l.append(i)
+
+        audio_dict = {}
+        for i in range(len(l)):
+            if i == 0:
+                print(i)
+                temp_audio = x[:l[i]]
+                sf.write(
+                    f'output/example/{upload_mp3.name[0:-4]}_{str(i)}.wav', temp_audio, sampleRate)
+            else:
+                print(i)
+                star = l[i-1]
+                temp_audio = x[star:l[i]]
+                sf.write(
+                    f'output/example/{upload_mp3.name[0:-4]}_{str(i)}.wav', temp_audio, sampleRate)
+
+            audio_dict[i] = temp_audio
+
+        r = sr.Recognizer()
+
+        a = []
+
+        for i in range(len(audio_dict)):
+            path = f'output/example/{upload_mp3.name[0:-4]}_{str(i)}.wav'
+            x1, sampleRate1 = librosa.load(path)
+
+            try:
+                with sr.AudioFile(path) as source:
+                    # listen for the data (load audio to memory)
+                    # Load audio ke memori
+                    audio_data = r.record(source)
+                    # recognize (convert from speech to text)
+                    # konversi audio ke teks dan menggunakan bahasa Indonesia (id), bahasa dapat diganti sesuai dengan kebutuhan
+                    text = r.recognize_google(audio_data, language='id')
+                    # Simpan teks
+                    a.append(text)
+
+            except:
+                print(path, ' kosong')
+
+        for i in a:
+            f = open(f"output/text/{upload_mp3.name[0:-4]}.txt", "a")
+            f.write(i+'\n')
+            f.close()
+
+    else:
+        st.write('Silahkan Upload File mp3')
--- web_st.py
+++ web_st.py
@@ -1,238 +1,62 @@
+import fungsi
+
 import streamlit as st
-import en_core_web_sm
-import PyPDF2
-import pdfplumber
-from pyresparser import ResumeParser
-from pydub import AudioSegment
-import librosa
-import soundfile as sf
-
-
-def seleksi():
-
-    posisi = 'Data analytics'
-
-    st.write('Page 1')
-
-    f1 = 'CV/CV.pdf'
-
-    data = ResumeParser(f1).get_extracted_data()
-
-    skills = data['skills']
-
-    skills = []
-
-    for i in data['skills']:
-
-        skills.append(i.lower())
-
-    terms = {'Quality/Six Sigma': ['black belt', 'capability analysis', 'control charts', 'doe', 'dmaic', 'fishbone',
-
-                                   'gage r&r', 'green belt', 'ishikawa', 'iso', 'kaizen', 'kpi', 'lean', 'metrics',
-
-                                   'pdsa', 'performance improvement', 'process improvement', 'quality',
-
-                                   'quality circles', 'quality tools', 'root cause', 'six sigma',
-
-                                   'stability analysis', 'statistical analysis', 'tqm'],
-
-             'Operations management': ['automation', 'bottleneck', 'constraints', 'cycle time', 'efficiency', 'fmea',
-
-                                       'machinery', 'maintenance', 'manufacture', 'line balancing', 'oee', 'operations',
-
-                                       'operations research', 'optimization', 'overall equipment effectiveness',
-
-                                       'pfmea', 'process', 'process mapping', 'production', 'resources', 'safety',
-
-                                       'stoppage', 'value stream mapping', 'utilization'],
-
-             'Supply chain': ['abc analysis', 'apics', 'customer', 'customs', 'delivery', 'distribution', 'eoq', 'epq',
-
-                              'fleet', 'forecast', 'inventory', 'logistic', 'materials', 'outsourcing', 'procurement',
-
-                              'reorder point', 'rout', 'safety stock', 'scheduling', 'shipping', 'stock', 'suppliers',
-
-                              'third party logistics', 'transport', 'transportation', 'traffic', 'supply chain',
-
-                              'vendor', 'warehouse', 'wip', 'work in progress'],
-
-             'Project management': ['administration', 'agile', 'budget', 'cost', 'direction', 'feasibility analysis',
-
-                                    'finance', 'kanban', 'leader', 'leadership', 'management', 'milestones', 'planning',
-
-                                    'pmi', 'pmp', 'problem', 'project', 'risk', 'schedule', 'scrum', 'stakeholders'],
-
-             'Data analytics': ['analytics', 'api', 'aws', 'big data', 'busines intelligence', 'clustering', 'code',
-
-                                'coding', 'data', 'database', 'data mining', 'data science', 'deep learning', 'hadoop',
-
-                                'hypothesis test', 'iot', 'internet', 'machine learning', 'modeling', 'nosql', 'nlp',
-
-                                'predictive', 'programming', 'python', 'r', 'sql', 'tableau', 'text mining',
-
-                                'visualuzation'],
-
-             'Healthcare': ['adverse events', 'care', 'clinic', 'cphq', 'ergonomics', 'healthcare',
-
-                            'health care', 'health', 'hospital', 'human factors', 'medical', 'near misses',
-
-                            'patient', 'reporting system']}
-
-    quality = 0
-
-    operations = 0
-
-    supplychain = 0
-
-    project = 0
-
-    data_science = 0
-
-    healthcare = 0
-
-    # # Create an empty list where the scores will be stored
-
-    scores = []
-    kemampuan = []
-
-    # Obtain the scores for each area
-
-    for area in terms.keys():
-
-        if area == 'Quality/Six Sigma':
-
-            for skill in terms[area]:
-
-                if skill in skills:
-                    kemampuan.append(skill)
-
-                    quality += 1
-
-            scores.append(quality)
-
-        elif area == 'Operations management':
-
-            for skill in terms[area]:
-
-                if skill in skills:
-                    kemampuan.append(skill)
-
-                    operations += 1
-
-            scores.append(operations)
-
-        elif area == 'Supply chain':
-
-            for skill in terms[area]:
-
-                if skill in skills:
-                    kemampuan.append(skill)
-
-                    supplychain += 1
-
-            scores.append(supplychain)
-
-        elif area == 'Project management':
-
-            for skill in terms[area]:
-
-                if skill in skills:
-                    kemampuan.append(skill)
-
-                    project += 1
-
-            scores.append(project)
-
-        elif area == 'Data analytics':
-
-            for skill in terms[area]:
-
-                if skill in skills:
-                    kemampuan.append(skill)
-
-                    data_science += 1
-
-            scores.append(data_science)
-
-        elif area == 'Healthcare':
-
-            for skill in terms[area]:
-
-                if skill in skills:
-                    kemampuan.append(skill)
-
-                    healthcare += 1
-
-            scores.append(healthcare)
-
-    st.write(f'Kemampuan yang dimiliki oleh {data["name"]} \n {"="*50}')
-
-    # st.write(data['name'])
-
-    ind = 0
-    job = {}
-
-    for x in terms.keys():
-
-        st.write(f'{x}: {scores[ind]}')
-        job[x] = scores[ind]
-
-        ind += 1
-
-    a = {}
-
-    a[data['name']] = [posisi, job[posisi], kemampuan]
-    st.write(a)
-
-    return a
-
-
-def stt():
-
-    song = AudioSegment.from_mp3('aziz1.mp3')
-
-    newSong = song
-
-    newSong.export('aziz1.wav', format="wav")
-
 
 '=========================================================================='
-
-
 # Create a page dropdown
+st.header('Aplikasi Seleksi CV')
+st.subheader('asd')
+
 
 page = st.selectbox("Pilih Menu", ["Seleksi CV", "Speech To Text"])
 
-
 if page == "Seleksi CV":
 
-    # Display details of page 1
+    from os import listdir
+    from os.path import isfile, join
 
-    genre = st.radio(
+    fungsi.ekstrak()
+    p_posisi = ['UI / UX', 'Back-end developer',
 
-        "Apakah ingin menambahkan data ? ",
 
-        ('Yes', 'No'))
+                'Front-end developer', 'Cloud Computing', 'Data analytics', 'Network Engineer', 'Mobile Developer']
 
-    if genre == 'Yes':
+    pil_posisi = st.selectbox('Pilih Posisi', p_posisi)
 
-        form = st.form(key='my_form')
+    but = st.button('Proses')
 
-        name = form.text_input(label='Enter some text')
+    if but:
 
-        submit_button = form.form_submit_button(label='Submit')
+        mypath = 'CV/'
+        allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        st.write(allfiles)
 
-        if submit_button:
+        list_orang = []
+        for filename in allfiles:
+            list_orang.append(fungsi.seleksi(filename, pil_posisi, p_posisi))
+            # list_orang.append(seleksi(filename))
 
-            st.write(f'hello {name}')
+        list_indeks = []
+        for i in list_orang:
+            indeks = list_orang.index(i)
+            sekor = i['skor']
+            list_indeks.append([indeks, sekor])
+        list_indeks.sort(key=lambda x: x[1], reverse=True)
 
-        # st.write('You selected comedy.')
+        list_ranked = []
+        for j in list_indeks:
+            for i in list_orang:
+                if j[0] == list_orang.index(i):
+                    list_ranked.append(i)
 
-    else:
-
-        # st.write("You didn't select comedy.")
-
-        seleksi()
+        ind = 1
+        for j in list_ranked:
+            a = ''
+            st.subheader(f'{ind}. {j["nama"]}')
+            # st.write(j['skil'])
+            a = ', '.join(j['skil'])
+            st.write('Skill :', a)
+            ind += 1
 
 
 elif page == "Speech To Text":
@@ -276,4 +100,7 @@ elif page == "Speech To Text":
     #     if submit_button:
 
     #         st.write(f'hello {name}')
-    pass
+    fungsi.stt()
+
+elif page == "Tes":
+    fungsi.tes()
